cb2042e9e7e1dbca6582f8fc1db816690dce36f6
卢伟锦 <60027985+CrazyDogJ@users.noreply.github.com>
2026-02-03 01:01:44 +0800

    update

---
+++ Source/ToonShader/Private/ToonFunctionLibrary.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ToonFunctionLibrary.h"
+
+void UToonFunctionLibrary::SetMaterialSlotsOverlayMaterial(USkeletalMeshComponent* MeshComponent,
+																 const TArray<UMaterialInterface*>& Materials)
+{
+	if (MeshComponent == nullptr)
+	{
+		return;
+	}
+	MeshComponent->MaterialSlotsOverlayMaterial = Materials;
+	MeshComponent->PrecachePSOs();
+	MeshComponent->MarkRenderStateDirty();
+}
+
+UMaterialInstanceDynamic* UToonFunctionLibrary::CreateAndSetOverlayMaterialInstanceDynamic(USkeletalMeshComponent* MeshComponent, int32 ElementIndex)
+{
+	TArray<TObjectPtr<class UMaterialInterface>> MaterialInterfaces;
+	MeshComponent->GetMaterialSlotsOverlayMaterial(MaterialInterfaces);
+	TObjectPtr<UMaterialInterface> MaterialInstance;
+	if (MaterialInterfaces.IsValidIndex(ElementIndex))
+	{
+		MaterialInstance = MaterialInterfaces[ElementIndex];
+	}
+	
+	UMaterialInstanceDynamic* MID = Cast<UMaterialInstanceDynamic>(MaterialInstance);
+
+	if (MaterialInstance && !MID)
+	{
+		// Create and set the dynamic material instance.
+		MID = UMaterialInstanceDynamic::Create(MaterialInstance, MeshComponent);
+		if (MeshComponent->MaterialSlotsOverlayMaterial.IsValidIndex(ElementIndex))
+		{
+			MeshComponent->MaterialSlotsOverlayMaterial[ElementIndex] = MID;
+		}
+	}
+
+	return MID;
+}
+
+void UToonFunctionLibrary::SetScalarParameterValueOnOverlayMaterials(USkeletalMeshComponent* MeshComponent,
+                                                                     const FName ParameterName, const float ParameterValue)
+{
+	TArray<TObjectPtr<class UMaterialInterface>> MaterialInterfaces;
+	MeshComponent->GetMaterialSlotsOverlayMaterial(MaterialInterfaces);
+	for (int32 MaterialIndex = 0; MaterialIndex < MaterialInterfaces.Num(); ++MaterialIndex)
+	{
+		if (UMaterialInterface* MaterialInterface = MaterialInterfaces[MaterialIndex])
+		{
+			UMaterialInstanceDynamic* DynamicMaterial = Cast<UMaterialInstanceDynamic>(MaterialInterface);
+			if (!DynamicMaterial)
+			{
+				DynamicMaterial = CreateAndSetOverlayMaterialInstanceDynamic(MeshComponent, MaterialIndex);
+			}
+			DynamicMaterial->SetScalarParameterValue(ParameterName, ParameterValue);
+		}
+	}
+}
---
+++ Source/ToonShader/Public/ToonFunctionLibrary.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ToonFunctionLibrary.generated.h"
+
+UCLASS()
+class TOONSHADER_API UToonFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Toon Shading")
+	static void SetMaterialSlotsOverlayMaterial(USkeletalMeshComponent* MeshComponent, const TArray<UMaterialInterface*>& Materials);
+
+	static UMaterialInstanceDynamic* CreateAndSetOverlayMaterialInstanceDynamic(USkeletalMeshComponent* MeshComponent, int32 ElementIndex);
+	
+	UFUNCTION(BlueprintCallable, Category = "Toon Shading")
+	static void SetScalarParameterValueOnOverlayMaterials(USkeletalMeshComponent* MeshComponent, const FName ParameterName, const float ParameterValue);
+};
